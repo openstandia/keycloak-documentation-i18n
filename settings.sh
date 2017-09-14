14d9063dc45b43821bc5d473c3143c2e7ae67297
Hiroyuki Wada <wadahiro@gmail.com>
2017-09-14 10:51:16 +0900

    Change lang ja => ja_JP for adapting to crowdin

--- settings.sh
+++ settings.sh
@@ -4,7 +4,7 @@ SOURCE_REPO=https://github.com/keycloak/keycloak-documentation.git
 SOURCE_REVISION=4cb5b9dd2daea2f61d372e39629b65013b21c57d
 SOURCE_DIR=source
 TRANSLATED_DIR=translated
-TARGET_LANG=ja
+TARGET_LANG=ja_JP
 DOCS=getting_started
 
 TARGET_EXT=adoc
---
+++ update-pot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0); pwd)
+. $DIR/settings.sh
+cd $DIR
+
+REPO_DIR=$DIR/$SOURCE_DIR
+
+# Clean all
+mkdir -p $REPO_DIR
+
+# Clone
+git clone --depth=1 $SOURCE_REPO $REPO_DIR
+cd $REPO_DIR && git checkout $SOURCE_REVISION
+git reset --hard HEAD && git clean -f
+
+# Update pot files
+cd $DIR
+type po4a
+if [ "$?" -eq 0 ]; then
+    po4a po4a.cfg
+else
+    docker run --rm -it -v $(pwd):/build -w /build -u $UID:$GID openstandia/keycloak-documentation po4a --no-translations --package-name="keycloak-documentation-i18n" --package-version=" " --copyright-holder="Nomura Research Institute, Ltd." po4a.cfg
+fi
+

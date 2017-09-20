6aee2656597caf5bbf3dcee6693232165348b124
Hiroyuki Wada <wadahiro@gmail.com>
2017-09-20 15:13:33 +0900

    Add variables doc as text

--- settings.sh
+++ settings.sh
@@ -18,4 +18,5 @@ DOCS="\
 TARGET_EXT=adoc
 OUT_FILE=po4a.cfg
 IGNORE_FILE=topics.adoc
+FORCE_TEXT_FILE="topics/templates/document-attributes-community.adoc|topics/templates/document-attributes-product.adoc"
 

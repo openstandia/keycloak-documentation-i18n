f07394b63827724be3ee79e19202b5f70f4511f0
Hiroyuki Wada <wadahiro@gmail.com>
2017-12-06 12:12:26 +0900

    Remove source position from POT files

--- update-po.sh
+++ update-po.sh
@@ -23,9 +23,10 @@ else
     docker run --rm -it -v $(pwd):/build -w /build -u $UID:$GID openstandia/keycloak-documentation-i18n:po4a-patch po4a --no-translations --package-name="keycloak-documentation-i18n" --package-version=" " --copyright-holder="Nomura Research Institute, Ltd." --msgmerge-opt '--previous' $@ po4a.cfg
 fi
 
-# Remove 'POT-Creation-Date'
+# Remove 'POT-Creation-Date', 'PO-Revision-Date' and source position
 find i18n/po/ -type f -name "*.po" | xargs sed -i -e "/^\"POT-Creation-Date/d"
 find i18n/po/ -type f -name "*.po" | xargs sed -i -e "/^\"PO-Revision-Date/d"
 find i18n/pot/ -type f -name "*.pot" | xargs sed -i -e "/^\"POT-Creation-Date/d"
 find i18n/pot/ -type f -name "*.pot" | xargs sed -i -e "/^\"PO-Revision-Date/d"
+find i18n/pot/ -type f -name "*.pot" | xargs sed -i -e "/^#: source\//d"
 

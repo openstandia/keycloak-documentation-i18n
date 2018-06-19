86355f54d3c0a4560c008f3804fb6ab70dc2123b
Hiroyuki Wada <wadahiro@gmail.com>
2018-06-19 10:09:22 +0900

    Fix including release_notes It needs to use fixed commit hash instead of 4.0.0.Final for work around.

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_REPO=https://github.com/keycloak/keycloak-documentation.git
-SOURCE_REVISION=4.0.0.Final
+SOURCE_REVISION=2d64137ab7c0edfe0fd248d35d55f2a8f082ab21
 SOURCE_DIR=source
 TRANSLATED_DIR=translated
 TARGET_LANG=ja_JP

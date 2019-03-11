7a370aac21a969b5448ea0c19ce92e4f316623f6
Hiroyuki Wada <wadahiro@gmail.com>
2019-03-11 11:11:36 +0000

    Update to 5.0.0

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_REPO=https://github.com/keycloak/keycloak-documentation.git
-SOURCE_REVISION=4.8.3.Final
+SOURCE_REVISION=5.0.0
 SOURCE_DIR=source
 TRANSLATED_DIR=translated
 TARGET_LANG=ja_JP

ff5f0ca0b980682fcddcdac5883a302efcc2c480
Hiroyuki Wada <wadahiro@gmail.com>
2021-12-27 21:52:01 +0900

    update to 15.0.2

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_REPO=https://github.com/keycloak/keycloak-documentation.git
-SOURCE_REVISION=14.0.0
+SOURCE_REVISION=15.0.2
 SOURCE_DIR=source
 TRANSLATED_DIR=translated
 TARGET_LANG=ja_JP

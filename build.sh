ffa2ec9b406e2018a32af6dbd137aaccf7c37c6a
Hiroyuki Wada <wadahiro@gmail.com>
2023-01-16 17:29:57 +0900

    [ci skip] Build changed versions only instead of full versions

--- build.sh
+++ build.sh
@@ -3,79 +3,94 @@
 DIR=$(cd $(dirname $0); pwd)
 cd $DIR
 
-DOCS="\
-  server_installation \
-  securing_apps \
-  server_admin \
-  server_development \
-  authorization_services \
-  upgrading \
-  release_notes"
+CHANGE_FILES=`echo $1 | tr ',' '\n'`
 
-TARGET_LANG=ja_JP
+echo "Change Files"
+echo "---------------"
+echo $CHANGE_FILES
+echo "---------------"
 
 BUILD_DIR=$DIR/build
 DIST_DIR=$DIR/dist
 
+echo "Clean $DIST_DIR"
+rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
-cp $BUILD_DIR/site/index.html $DIST_DIR/
-cp $BUILD_DIR/site/*.png $DIST_DIR/
 
+echo "Clean $BUILD_DIR"
+rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
+
+echo "Clone keycloak-documentation"
 git clone https://github.com/keycloak/keycloak-documentation $BUILD_DIR
 cd $BUILD_DIR
 
-for version in `ls $DIR/src`; do
-  echo "Checkout $version"
+for language in `ls $DIR/translations`; do
+  # Check the target language files is changed
+  echo $CHANGE_FILES | grep -q translations/$language
 
-  git reset --hard && git clean -xf
-  git checkout $version
+  if [[ $? -eq 0 ]]; then
+    for version in `ls $DIR/translations/$language`; do
+      # Check the target version files is changed
+      echo $CHANGE_FILES | grep -q translations/$language/$version
 
-  for docname in $DOCS; do
-    TARGET=$docname/index.adoc
+      if [[ $? -eq 0 ]]; then
+        echo "Checkout $version"
 
-    if [ -f $TARGET ]; then
-      echo "Build $docname"
+        git reset --hard && git clean -xf
+        git checkout $version
 
-      asciidoctor \
-        -b html5 \
-        -r asciidoctor-i18n \
-        -a source-highlighter=coderay \
-        -a imagesdir=./ \
-        -a toc=left \
-        -a icons=font \
-        -a sectanchors=true \
-        -a idprefix \
-        -a idseparator=- \
-        -a docinfo1=true \
-        -a project_buildType=archive \
-        -a language=$docname \
-        -a po-directory=$DIR/translations/$TARGET_LANG/$version/ \
-        -a toc-title=目次 \
-        -a nofooter=true \
-        -a project_doc_base_url=link:.. \
-        $TARGET
+        for pofile in `ls $DIR/src/$version`; do
+          docname=`echo $pofile | sed "s/\.po//"`
+          TARGET=$docname/index.adoc
 
-      DOC_VERSION_DIR=$DIST_DIR/$version
-      DOC_DIST_DIR=$DOC_VERSION_DIR/$TARGET_LANG/$docname
-      echo "Copy resources to $DOC_DIST_DIR"
+          echo "Build $language/$version/$docname"
 
-      mkdir -p $DOC_DIST_DIR
-      rm -rf $DOC_DIST_DIR/*
+          asciidoctor \
+            -b html5 \
+            -r asciidoctor-i18n \
+            -a source-highlighter=coderay \
+            -a imagesdir=./ \
+            -a toc=left \
+            -a icons=font \
+            -a sectanchors=true \
+            -a idprefix \
+            -a idseparator=- \
+            -a docinfo1=true \
+            -a project_buildType=archive \
+            -a language=$docname \
+            -a po-directory=$DIR/translations/$language/$version/ \
+            -a toc-title=目次 \
+            -a nofooter=true \
+            -a project_doc_base_url=link:.. \
+            $TARGET
 
-      cp $docname/index.html $DOC_DIST_DIR/
+          if [[ $? -ne 0 ]]; then
+            echo "Failed to build: $language/$version/$docname"
+            exit 1
+          fi
 
-      if [ -d $docname/images ]; then
-        cp -r $docname/images $DOC_DIST_DIR/
+          DOC_VERSION_DIR=$DIST_DIR/$version
+          DOC_DIST_DIR=$DOC_VERSION_DIR/$language/$docname
+          echo "Copy resources to $DOC_DIST_DIR"
+
+          mkdir -p $DOC_DIST_DIR
+
+          cp $docname/index.html $DOC_DIST_DIR/
+
+          if [ -d $docname/images ]; then
+            cp -r $docname/images $DOC_DIST_DIR/
+          fi
+
+          if [ -d $docname/keycloak-images ]; then
+            cp -r $docname/keycloak-images $DOC_DIST_DIR/
+          fi
+
+          sed -e "s|@VERSION@|$version|g" $DIR/site/index.html.tmpl > $DOC_VERSION_DIR/index.html
+          cp $DIR/site/*.png $DOC_VERSION_DIR/
+        done
       fi
-
-      if [ -d $docname/keycloak-images ]; then
-        cp -r $docname/keycloak-images $DOC_DIST_DIR/
-      fi
-
-      sed -e "s|@VERSION@|$version|g" $DIR/site/index.html.tmpl > $DOC_VERSION_DIR/index.html
-      cp $DIR/site/*.png $DOC_VERSION_DIR/
-    fi
-  done
+    done
+  fi
 done
 
